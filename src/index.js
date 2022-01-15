a07b309eb57c01411292dc1eaa0461b2ccb6571f
Andrei <bondarevandre@gmail.com>
2022-01-16 00:53:41 +0300

    feat:add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    let d = String(n);
+
+    let arr = [];
+    arr = d.split("");
+    arr.reverse();
+    d = arr.join("");
+    d = Number(d);
+    return d;
 }
