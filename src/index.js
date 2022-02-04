7837a4c9602c795b6a4e130dac13ffc1f50b24d6
Andrei <bondarevandre@gmail.com>
2022-02-04 22:36:30 +0300

    feat:change code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 module.exports = function reverse (n) {
     n = Math.abs(n);
-    let d = String(n);
-
-    let arr = [];
-    arr = d.split("");
-    arr.reverse();
-    d = arr.join("");
-    d = Number(d);
-    return d;
-}
+      let d = String(n);
+    let i = 0;
+   
+    let result = '';
+    while (i < d.length) {
+      // Соединяем в обратном порядке
+      result = d[i] + result;
+     
+      i = i + 1;
+    }
+ 
+    return Number(result);
+  };
